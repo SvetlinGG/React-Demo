67d28a65c3b562a622ceca2d9afd64ad1ec5f46a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-29 23:48:46 +0100

    create Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import Header from "../components/Header"
 
-import './App.css'
 
 function App() {
   return (
@@ -16,56 +16,7 @@ function App() {
   </div>
   {/* ***** Preloader End ***** */}
   {/* ***** Header Area Start ***** */}
-  <header
-    className="header-area header-sticky wow slideInDown"
-    data-wow-duration="0.75s"
-    data-wow-delay="0s"
-  >
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <nav className="main-nav">
-            {/* ***** Logo Start ***** */}
-            <a href="index.html" className="logo">
-              <img src="assets/images/logo.png" alt="Chain App Dev" />
-            </a>
-            {/* ***** Logo End ***** */}
-            {/* ***** Menu Start ***** */}
-            <ul className="nav">
-              <li className="scroll-to-section">
-                <a href="#top" className="active">
-                  Home
-                </a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#services">Services</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#about">About</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#pricing">Pricing</a>
-              </li>
-              <li className="scroll-to-section">
-                <a href="#newsletter">Newsletter</a>
-              </li>
-              <li>
-                <div className="gradient-button">
-                  <a id="modal_trigger" href="#modal">
-                    <i className="fa fa-sign-in-alt" /> Sign In Now
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <a className="menu-trigger">
-              <span>Menu</span>
-            </a>
-            {/* ***** Menu End ***** */}
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+  <Header />
   {/* ***** Header Area End ***** */}
   <div id="modal" className="popupContainer" style={{ display: "none" }}>
     <div className="popupHeader">
