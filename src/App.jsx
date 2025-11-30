0a6529fabb05b11f6dca48360edf813af5c05347
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-30 01:29:59 +0100

    setup again the app.jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
-import About from "../components/About"
-import Banner from "../components/Banner"
-import Clients from "../components/Clients"
-import Footer from "../components/Footer"
 import Header from "../components/Header"
 import HeaderArea from "../components/HeaderArea"
-import Pricing from "../components/Pricing"
+import Banner from "../components/Banner"
 import Services from "../components/Services"
-
+import About from "../components/About"
+import Clients from "../components/Clients"
+import Pricing from "../components/Pricing"
+import Footer from "../components/Footer"
 
 function App() {
   return (
     <>
-  <div id="js-preloader" className="js-preloader">
-    <div className="preloader-inner">
-      <span className="dot" />
-      <div className="dots">
-        <span />
-        <span />
-        <span />
-      </div>
-    </div>
-  </div>
-  {/* ***** Preloader End ***** */}
-  {/* ***** Header Area Start ***** */}
-  <Header />
-  {/* ***** Header Area End ***** */}
-  <HeaderArea />
-  <Banner />
-  <Services />
-  <About />
-  <Clients />
-  <Pricing />
-  <Footer />
-</>
-
+      <Header />
+      <HeaderArea />
+      <Banner />
+      <Services />
+      <About />
+      <Clients />
+      <Pricing />
+      <Footer />
+    </>
   )
 }
 
