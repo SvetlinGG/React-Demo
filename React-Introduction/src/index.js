a7aaf448162d672e368a4968c49f7af84dea625b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-02-28 23:29:31 +0100

    render and create React element

--- React-Introduction/src/index.js
+++ React-Introduction/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootHtmlElement = document.getElementById('root');
+
+const rootReactElement = ReactDOM.createRoot(rootHtmlElement);
+
+const reactElement = React.createElement('h1', {}, 'Hello World!');
+
+rootReactElement.render(reactElement);
+
