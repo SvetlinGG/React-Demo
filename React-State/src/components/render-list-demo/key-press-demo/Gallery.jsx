a65bba8704154bde1d2c540ee95d9cd4c6f239fd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-06 13:20:53 +0100

    key down handler

--- React-State/src/components/render-list-demo/key-press-demo/Gallery.jsx
+++ React-State/src/components/render-list-demo/key-press-demo/Gallery.jsx
@@ -1,13 +1,19 @@
 
 export default function Gallery(){
 
+    const onKeyDownHandler = (e) => {
+        console.log(e.key);
+        console.log('Key Down');
+        
+        
+    }
 
-    return(
+    return (
         <>
-        <h2>Gallery demo (key press)</h2>
-
-        <div>
-            <h3>Gallery view</h3>
+        <h2>Gallery Demo (key press)</h2>
+        <div style={{backgroundColor: 'lightblue'}}>
+            <h3>Gallery View</h3>
+            <img style={{width:200}} src="https://img.freepik.com/premium-photo/illustration-cute-boy-avatar-graphic-white-background-created-with-generative-ai-technology_67092-4578.jpg?w=2000" alt="" />
         </div>
         </>
     )
